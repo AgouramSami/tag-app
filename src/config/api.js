9b01c096cbd709607d4d352c43133428a48cb63a
AgouramSami <samietwassim@gmail.com>
2025-03-27 01:54:28 +0100

    Add default API URL and debug log

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://tag-app-production.up.railway.app/api";
+console.log("API_URL:", API_URL);
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
